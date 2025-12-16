480992dacc729cd499ffe70435ff16b53917256a
Leander Fernandes <leanderfdes22@gmail.com>
2025-12-16 23:21:01 +0530

    chore: added aboutSection.ts to sanity folder and entered about title uploaded a pic and description

---
+++ sanity/schemas/aboutSection.ts
@@ -0,0 +1,33 @@
+export default {
+  name: "aboutSection",
+  title: "About Section",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Section Title",
+      type: "string",
+      initialValue: "About the Studio",
+      validation: (R: any) => R.required()
+    },
+    {
+      name: "content",
+      title: "Story",
+      type: "array",
+      of: [{ type: "block" }],
+      validation: (R: any) => R.required()
+    },
+    {
+      name: "image",
+      title: "About Image",
+      type: "image",
+      options: { hotspot: true }
+    },
+    {
+      name: "visible",
+      title: "Visible",
+      type: "boolean",
+      initialValue: true
+    }
+  ]
+};
--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,9 +1,11 @@
 import siteSettings from "./siteSettings";
+import aboutSection from "./aboutSection";
 import collection from "./collection";
 import order from "./order";
 
 export default [
   siteSettings,
+  aboutSection,
   collection,
   order
 ];
